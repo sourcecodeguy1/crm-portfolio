3ff73c3f4129b28ba71a733c0a60201b76feac13
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2025-03-02 17:27:42 -0800

    update client dockerfile

---
+++ client/src/app/interfaces/environment.model.ts
@@ -0,0 +1,5 @@
+export interface EnvironmentModel {
+  production: boolean;
+  apiUrl: string;
+  googleAnalyticsId: string;
+}
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,37 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+// Declare dataLayer on the Window interface
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch(err => console.error(err));
+
+function loadGoogleAnalytics() {
+  if (environment.googleAnalyticsId) {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${environment.googleAnalyticsId}`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag(...args: any) {
+      window.dataLayer.push(args);
+    }
+
+    gtag('js', new Date());
+    gtag('config', environment.googleAnalyticsId);
+  } else {
+    console.warn('Google Analytics ID not found in environment.prod.ts');
+  }
+}
+
+// Call the function on app startup
+loadGoogleAnalytics();
