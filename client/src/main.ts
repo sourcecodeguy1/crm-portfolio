aeca90f1d5c34534aaa76675b15721d148b404f9
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2025-06-02 00:41:12 -0700

    add XSRF-TOKEN

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,22 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { GoogleAnalyticsService } from './app/services/google-analytics.service';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
+import { AuthInterceptor } from './app/interceptors/auth.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideHttpClient(
+      withInterceptors([AuthInterceptor]),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      })
+    ),
+  ],
+})
   .then(appRef => {
     // Get Google Analytics service from the injector after bootstrap
     const gaService = appRef.injector.get(GoogleAnalyticsService);
