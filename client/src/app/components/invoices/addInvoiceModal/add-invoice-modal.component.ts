252886fd507f50eb602801014d65751a163aaca6
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2026-04-30 21:57:15 -0700

    feat: replace client ID input with dropdown in invoice modals

--- client/src/app/components/invoices/addInvoiceModal/add-invoice-modal.component.ts
+++ client/src/app/components/invoices/addInvoiceModal/add-invoice-modal.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ClientService } from '../../../services/client.service';
+import { Observable } from 'rxjs';
+import { Client } from '../../../interfaces/client.interface';
 
 @Component({
   selector: 'app-add-invoice-modal',
@@ -8,10 +11,14 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./add-invoice-modal.component.scss'],
   imports: [CommonModule, ReactiveFormsModule]
 })
-export class AddInvoiceModalComponent {
+export class AddInvoiceModalComponent implements OnInit {
   @Output() invoiceAdded = new EventEmitter<any>();
   @Output() closed = new EventEmitter<void>();
   invoiceForm: FormGroup;
+  clients$: Observable<Client[]>;
+
+  private clientService = inject(ClientService);
+
   constructor(private fb: FormBuilder) {
     this.invoiceForm = this.fb.group({
       client_id: ['', [Validators.required]],
@@ -19,7 +26,11 @@ export class AddInvoiceModalComponent {
       due_date: ['', [Validators.required]],
       status: ['pending']
     });
+    this.clients$ = this.clientService.getClients();
   }
+
+  ngOnInit(): void {}
+
   submit() {
     if (this.invoiceForm.valid) {
       this.invoiceAdded.emit(this.invoiceForm.value);
--- client/src/app/components/invoices/editInvoiceModal/edit-invoice-modal.component.ts
+++ client/src/app/components/invoices/editInvoiceModal/edit-invoice-modal.component.ts
@@ -1,7 +1,10 @@
-import { Component, EventEmitter, Output, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnChanges, SimpleChanges, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Invoice } from '../../../interfaces/invoice.interface';
+import { ClientService } from '../../../services/client.service';
+import { Observable } from 'rxjs';
+import { Client } from '../../../interfaces/client.interface';
 
 @Component({
   selector: 'app-edit-invoice-modal',
@@ -9,11 +12,14 @@ import { Invoice } from '../../../interfaces/invoice.interface';
   templateUrl: './edit-invoice-modal.component.html',
   styleUrl: './edit-invoice-modal.component.scss'
 })
-export class EditInvoiceModalComponent implements OnChanges {
+export class EditInvoiceModalComponent implements OnChanges, OnInit {
   @Input() invoice: Invoice | null = null;
   @Output() invoiceEdited = new EventEmitter<any>();
   @Output() closed = new EventEmitter<void>();
   invoiceForm: FormGroup;
+  clients$: Observable<Client[]>;
+
+  private clientService = inject(ClientService);
 
   constructor(private fb: FormBuilder) {
     this.invoiceForm = this.fb.group({
@@ -22,7 +28,10 @@ export class EditInvoiceModalComponent implements OnChanges {
       due_date: ['', [Validators.required]],
       status: ['pending']
     });
+    this.clients$ = this.clientService.getClients();
   }
+
+  ngOnInit(): void {}
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['invoice'] && this.invoice) {
